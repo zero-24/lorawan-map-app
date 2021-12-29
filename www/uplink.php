4c1fbfc755f123d9ae988a3da9d14022ecc31f3b
zero-24 <zero-24@users.noreply.github.com>
2021-12-29 11:38:48 +0100

    handle multible tracker sending data

--- www/uplink.php
+++ www/uplink.php
@@ -13,22 +13,35 @@ if ($input->getString('uplink_secret', false) !== UPLINK_SECRET)
 	exit;
 }
 
+// Read current gps data
+$currentGpsData = $fileHelper->readJsonFile('gps_data');
+
 // Read data
 $dataUplinkMessages = (array) $input->json->get('uplink_message', array());
 $dataDevice = (array) $input->json->get('end_device_ids', array());
 
-// Write data to an stdClass object
+// loop through the current data
+foreach ($currentGpsData as $currentGpsPoint)
+{
+	if ($currentGpsPoint['device_id'] !== $dataDevice['device_id'])
+	{
+		$gpsData[] = $currentGpsPoint;
+	}
+}
+
+// Write new data to an stdClass object
 $tracker = new stdClass;
 
+// Take the data from the payload
 foreach ($dataUplinkMessages['decoded_payload'] as $key => $value)
 {
-    $tracker->$key = $value;
+	$tracker->$key = $value;
 }
 
 $tracker->time = date("d-m-Y H:i:s");
 $tracker->device_id = $dataDevice['device_id'];
 
-// In the end we need an array of tracker elements
+// Append the new / updated tracker data to the array
 $gpsData[] = $tracker;
 
 // Encode the json
